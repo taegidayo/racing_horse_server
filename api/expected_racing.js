fd0032dccd04a34227c9fbe7c62b6860de42f26f
alwaystaegi <ffff4565@naver.com>
2023-10-21 00:15:06 +0900

    api 1추가

---
+++ api/expected_racing.js
@@ -0,0 +1,17 @@
+import { MongoClient } from "mongodb";
+import "dotenv/config";
+
+module.exports = async (req, res) => {
+  const client = new MongoClient(process.env.MONGO_URL);
+
+  const database = client.db("project_hr");
+  const col = await database.collection("expected_racing");
+
+  const result = col.find({});
+
+  client.close();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.status(200).send(result);
+};
