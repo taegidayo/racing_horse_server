fbfd0ae4a4a47d8f3abe939230da5804ae0fc921
alwaystaegi <ffff4565@naver.com>
2023-10-21 01:13:21 +0900

    mongodb에서 읽어오는 방식 수정

--- api/expected_day.js
+++ api/expected_day.js
@@ -30,5 +30,5 @@ module.exports = async (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.status(200).send({ distinctValues });
+  res.status(200).send(distinctValues);
 };
