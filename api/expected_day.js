a6790a9599ba9e976e96ff29cb0563a59f31a0cc
alwaystaegi <ffff4565@naver.com>
2023-10-21 01:03:53 +0900

    api 수정

--- api/expected_day.js
+++ api/expected_day.js
@@ -5,13 +5,14 @@ module.exports = async (req, res) => {
   const client = new MongoClient(process.env.MONGO_URL);
 
   const database = client.db("project_hr");
-  const result = await database
-    .collection("expected_racing_summary")
-    .distinct("rcDate");
+  const col = database.collection("expected_racing_summary");
+
+  const result_date = await col.distinct("rcDate");
+  const result_day = await col.distinct("rcDay");
 
   client.close();
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.status(200).send(result);
+  res.status(200).send({ date: result_date, day: result_day });
 };
