288e031b05ec1fb5c330ebb059fcd1eecde2aebb
alwaystaegi <ffff4565@naver.com>
2023-10-21 01:11:49 +0900

    mongodb에서 읽어오는 방식 수정

--- api/expected_day.js
+++ api/expected_day.js
@@ -7,12 +7,28 @@ module.exports = async (req, res) => {
   const database = client.db("project_hr");
   const col = database.collection("expected_racing_summary");
 
-  const result_date = await col.distinct("rcDate");
-  const result_day = await col.distinct("rcDay");
+  const results = await col
+    .aggregate([
+      {
+        $group: {
+          _id: {
+            rcDate: "$rcDate",
+            rcDay: "$rcDay",
+          },
+        },
+      },
+    ])
+    .toArray();
+
+  // 결과에서 데이터를 추출
+  const distinctValues = results.map((item) => ({
+    rcDate: item._id.rcDate,
+    rcDay: item._id.rcDay,
+  }));
 
   client.close();
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.status(200).send({ date: result_date, day: result_day });
+  res.status(200).send({ distinctValues });
 };
