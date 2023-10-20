45f6583995339cba8ff1f01460a9f5d48d7cc862
alwaystaegi <ffff4565@naver.com>
2023-10-20 23:44:48 +0900

    dbtest

--- api/expected_day.js
+++ api/expected_day.js
@@ -5,9 +5,9 @@ module.exports = async (req, res) => {
   const client = new MongoClient(process.env.MONGO_URL);
 
   const database = client.db("project_hr");
-  const col = database.collection("expected_racing");
-
-  var result = await col.find({}).toArray();
+  const result = await database
+    .collection("expected_racing")
+    .distinct("rcDate");
 
   client.close();
   res.status(200).send(result);
