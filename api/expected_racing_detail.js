75c910fab0d7d6149fd643b016f295694cd2cdce
alwaystaegi <ffff4565@naver.com>
2023-10-21 21:40:03 +0900

    api test

--- api/expected_racing_detail.js
+++ api/expected_racing_detail.js
@@ -24,7 +24,7 @@ module.exports = async (req, res) => {
   // console.log(param.time)
 
   // if (result) {
-  res.status(200).send(param);
+  res.status(200).send({ param: param, result: result });
   // } else {
   // res.status(200).send({ dd: "bye" });
   // }
