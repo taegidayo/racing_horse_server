f5d7c5235f171e03f474569c90c9efbba20128c9
alwaystaegi <ffff4565@naver.com>
2023-10-22 19:06:54 +0900

    모든 말 이름 api

--- api/hrName.js
+++ api/hrName.js
@@ -5,12 +5,9 @@ module.exports = async (req, res) => {
   const client = new MongoClient(process.env.MONGO_URL);
 
   const database = client.db("project_hr");
-  const col = database.collection("expected_racing_summary");
+  const col = database.collection("racing_result");
 
-  // hrName 필드의 모든 고유한 값을 가져옵니다.
   const distinctHrNames = await col.distinct("hrName");
-
-  // console.log(distinctHrNames);
 
   client.close();
   res.setHeader("Access-Control-Allow-Origin", "*");
