69a23dad3b43bc8da5269c21d6e75b237eb8a566
alwaystaegi <ffff4565@naver.com>
2023-10-22 14:12:15 +0900

    경주결과 api

---
+++ api/racing_result_summary.js
@@ -0,0 +1,17 @@
+import { MongoClient } from "mongodb";
+import "dotenv/config";
+
+module.exports = async (req, res) => {
+  const client = new MongoClient(process.env.MONGO_URL);
+
+  const database = client.db("project_hr");
+  const col = database.collection("expected_racing_summary");
+
+  const result = await col.find({}).toArray();
+
+  client.close();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.status(200).send(result);
+};
