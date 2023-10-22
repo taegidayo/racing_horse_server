b5b5d31f7c93d3d81399bec97a45434702587c3f
alwaystaegi <ffff4565@naver.com>
2023-10-22 19:21:38 +0900

    검색api

---
+++ api/search_hrName.js
@@ -0,0 +1,29 @@
+import { MongoClient } from "mongodb";
+import "dotenv/config";
+
+module.exports = async (req, res) => {
+  const client = new MongoClient(process.env.MONGO_URL);
+
+  const param = req.query;
+
+  const database = client.db("project_hr");
+  const col = database.collection("racing_result");
+
+  const result = await col
+    .find({
+      hrName: param.hrName,
+    })
+    .toArray();
+
+  client.close();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  // console.log(param.time)
+
+  // if (result) {
+  res.status(200).send(result);
+  // } else {
+  // res.status(200).send({ dd: "bye" });
+  // }
+};
